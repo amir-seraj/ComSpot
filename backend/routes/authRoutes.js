c8866488fea950d1cddd74076421be8be900db4f
Amirserajj <amirseraj.ir@gmail.com>
2022-07-10 14:34:21 +0430

    request validation setup for login and register with Joi in authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
 import { postRegister, postLogin } from "../controllers/auth/authController.js";
+import Joi from "joi";
+import validator from "express-joi-validation";
 
+const authValidator = validator.createValidator({});
+
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
+  email: Joi.string().email().required(),
+});
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12).required(),
+  email: Joi.string().email().required(),
+});
 const router = express.Router();
 
-router.post("/register", postRegister);
+router.post("/register", authValidator.body(registerSchema), postRegister);
 
-router.post("/login", postLogin);
+router.post("/login", authValidator.body(loginSchema), postLogin);
 
 export default router;
