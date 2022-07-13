665f1e34e765663f56b7a7e3af46583f6d415b8b
Amirserajj <amirseraj.ir@gmail.com>
2022-07-13 12:07:55 +0430

    Add token to requests, setup logout in utils

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,24 @@
 import axios from "axios";
+import { logout } from "./auth";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:5002/api",
   timeout: 1000,
 });
+apiClient.interceptors.request.use(
+  (config) => {
+    const userDetails = localStorage.getItem("user");
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+// Public Route
 export const login = async (data) => {
   try {
     return await apiClient.post("/auth/login", data);
@@ -18,4 +33,13 @@ export const register = async (data) => {
   try {
     return await apiClient.post("/auth/register", data);
   } catch (exception) {}
+};
+
+//Private Routes
+
+const checkResponse = (exception) => {
+  const responseCode = exception?.response?.status;
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
+  }
 };
